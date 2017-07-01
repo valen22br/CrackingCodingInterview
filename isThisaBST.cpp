6bff20fb6be94156c201436463a49326386000b3
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2017-07-01 10:46:10 -0400

    Update isThisaBST.cpp

--- isThisaBST.cpp
+++ isThisaBST.cpp
@@ -10,7 +10,7 @@ The Node struct is defined as follows:
    int printPostOrder(Node* root)
    {
        if(root == NULL)
-          return 0;
+          return -99;
        printPostOrder(root->left);
        printPostOrder(root->right);
        cout << "Values: " << root->data << endl;
@@ -19,37 +19,43 @@ The Node struct is defined as follows:
        
    }
 
-   int printInorder(Node* root)
-   {
-      if(root == NULL)
-          return 0;
-       printInorder(root->left);
-       cout << "Values: " << root->data << endl;
-       printInorder(root->right);
-       
-       return root->data;
 
+
+   bool isBST(Node* root, Node* left = NULL, Node*right = NULL)
+   {
        
+       // Base condition
+    if (root == NULL)
+        return true;
+ 
+    // if left node exist that check it has
+    // correct data or not
+    if (left != NULL and root->data < left->data)
+        return false;
+ 
+    // if right node exist that check it has
+    // correct data or not
+    if (right != NULL and root->data > right->data)
+        return false;
+ 
+    // check recursively for every node.
+    return isBST(root->left, left, root) and
+           isBST(root->right, root, right);
    }
 
 
    bool checkBST(Node* root) {
-       bool isBST = true;
+       bool isBSTree = true;
        Node* x = new Node();
        x = root;
        
-       cout << "PostOrder: "<< endl;
-       printPostOrder(x);
+       //cout << "PostOrder: "<< endl;
+       //printPostOrder(x);
        
-       cout << "InOrder: "<< endl;
-       printInorder(x);
+       //cout << "InOrder: "<< endl;
+       isBSTree = isBST(x);
        //checkRight(x);
        
-       int i = 0;
-       while (i <= 10000)
-       {
-           if(x->data )
-           i++;
-       }
-       return isBST;
+
+       return isBSTree;
    }
