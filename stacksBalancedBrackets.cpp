4b019995ff8b2f8ed05933e1713033fbf3d3b8cf
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2017-07-01 18:57:32 -0400

    Update stacksBalancedBrackets.cpp

--- stacksBalancedBrackets.cpp
+++ stacksBalancedBrackets.cpp
@@ -27,65 +27,20 @@ stack<int> balanced;
 
 
 bool is_balanced(string expression) {
-   int values[126];
-    for (int i = 0; i < 126; i++)
-    {
-        values[i] = 0;
+  stack<char> s;
+  for (char c : expression) {
+    if      (c == '{') s.push('}');
+    else if (c == '[') s.push(']');
+    else if (c == '(') s.push(')');
+    else {
+      if (s.empty() || c != s.top())
+        return false;
+      s.pop();
     }
-    
-    
-    for(int i = 0; i < expression.length(); i++)
-    {
-        balanced.push(expression[i]);
-        
-    }
-    
-    //cout << balanced.size() << endl;
-    bool isValid = 0;
-    int topResult1, topResult2 = 0;
-    int stackMiddleSize = balanced.size() / 2;
-    cout << "stack: " << endl;
-   while(balanced.size() != 0)
-   {
-       
-       topResult1 = balanced.top();
-       balanced.pop();
-       topResult2 = balanced.top();
-       balanced.pop();
-      
-       
-       cout << topResult1 << " => "<< topResult1 / topResult2 <<   endl;
-       cout << topResult2 << endl;
-       
-       /*
-       
-       if(topResult1 <= topResult2 && stackMiddleSize >=0)
-           cout << topResult1 << " <= " << topResult2 << " topResult1 <= topResult2 " << endl;
-           //return 0;
-       
-       if(topResult1 > topResult2 && stackMiddleSize <0)
-           cout << "topResult1 > topResult2 " << endl;
-           //return 0;
-
-       stackMiddleSize--;       
-       
-       //balanced.pop();
-       
-       */
-   }
-    
-    if(balanced.size() %2 == 0)
-        return 0;
-    
-
-    
-
-    
-
-    
-    
-    return 0;
+  }
+  return s.empty();
 }
+ 
 
 int main(){
     int t;
