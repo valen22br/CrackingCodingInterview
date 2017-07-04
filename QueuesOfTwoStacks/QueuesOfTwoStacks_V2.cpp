5e35209cbd578742a305090f10a5372129515e9a
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2017-07-03 22:15:45 -0400

    Create QueuesOfTwoStacks_V2.cpp

---
+++ QueuesOfTwoStacks/QueuesOfTwoStacks_V2.cpp
@@ -0,0 +1,77 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class MyQueue {
+
+    public:
+        stack<int> s1;
+        stack<int> s2;
+        void push(int x)
+        {
+            s1.push(x);
+        }
+
+        void pop()
+        {
+            if(s2.empty()==true)
+            {
+                if(s1.empty()==true) return;
+                else
+                {
+                    while(!s1.empty())
+                    {
+                        s2.push(s1.top());
+                        s1.pop();
+                    }
+
+                }
+
+            }
+
+            s2.pop();
+
+        }
+
+        int front()
+        {
+            if(!s2.empty()) return s2.top();
+            else
+            {
+                if(!s1.empty())
+                {
+                    while(!s1.empty())
+                    {
+                        s2.push(s1.top());
+                        s1.pop();
+                    }
+                }
+            }
+            return s2.top();
+        }
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
