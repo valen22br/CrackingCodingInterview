104859c7755a09ff2c202c7064636ff3814e6c92
Luis Gustavo Grubert Valensuela <valensuela1@gmail.com>
2017-10-07 12:24:57 -0400

    Create stringManipulation.cpp

---
+++ GG/stringManipulation.cpp
@@ -0,0 +1,98 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+#include<queue>
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<cstdio>
+string solution(string &S, int K) {
+    // write your code in C++14 (g++ 6.2.0)
+    string ss = "";
+    queue <char> qdash;
+    queue <char> qchars;
+    
+    for(unsigned int i = 0; i < S.length(); i++)
+    {
+        if(S[i] == '-')
+        {
+            qdash.push(S[i]);
+        }
+        else
+        {
+            qchars.push(S[i]);   
+        }
+            
+        
+    }
+    
+    /*cout << "K Size:  " << K << endl;
+    cout << "Dashs:  " <<   qdash.size() << endl;
+    cout << "chars:  " <<   qchars.size() << endl;
+    */
+   if(K == 1)
+   {
+       while(!qchars.empty())
+       {
+           ss += static_cast<char>(toupper(qchars.front()));
+           qchars.pop();
+       }
+   }
+   
+   else if(qchars.size() % K == 0)
+   {
+     int groups = qchars.size()/K;
+     //cout << "zero" << endl;   
+     int count = 0;
+     while(!qchars.empty())
+       {
+           count ++;
+           ss += static_cast<char>(toupper(qchars.front()));
+           qchars.pop();
+           if(count == K)
+           {
+               groups --;
+               count = 0;
+               if(groups > 0)
+               {
+                    ss += '-';
+               }
+           }
+       }
+     
+   }
+   else if(qchars.size() % K != 0)
+   {
+        int firstGroupLeght = qchars.size()%K;
+        for (int i = firstGroupLeght; i > 0; i--)
+        {
+            ss += qchars.front();
+            qchars.pop();
+            
+        }
+        ss += '-';
+        
+        int groups = qchars.size()/K;
+         //cout << "zero" << endl;   
+         int count = 0;
+         while(!qchars.empty())
+           {
+               count ++;
+               ss += static_cast<char>(toupper(qchars.front()));
+               qchars.pop();
+               if(count == K)
+               {
+                   groups --;
+                   count = 0;
+                   if(groups > 0)
+                   {
+                        ss += '-';
+                   }
+               }
+           }
+   }
+    
+    return ss;
+}
